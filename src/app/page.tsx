22830f293d27e09271bdc47819eb6b5e4ed22657
Ahmed Hinedy <ahmedhinedy@gmail.com>
2024-11-29 02:06:13 +0200

    revamp initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,155 @@
+import Markdown from "react-markdown";
+
+import Link from "next/link";
+import { DATA } from "@/data/resume";
+import { getBlogPosts } from "@/data/blog";
+import { Badge } from "@/components/ui/badge";
+import BlurFade from "@/components/magicui/blur-fade";
+import { ResumeCard } from "@/components/resume-card";
+import BlurFadeText from "@/components/magicui/blur-fade-text";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const BLUR_FADE_DELAY = 0.04;
+
+export default async function Page() {
+  const posts = await getBlogPosts();
+
+  return (
+    <main className="flex flex-col min-h-[100dvh] space-y-10">
+      <section id="hero">
+        <div className="mx-auto w-full max-w-2xl space-y-8">
+          <div className="gap-2 flex justify-between">
+            <div className="flex-col flex flex-1 space-y-1.5">
+              <BlurFadeText
+                delay={BLUR_FADE_DELAY}
+                className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                yOffset={8}
+                text={`${DATA.name}`}
+              />
+              <BlurFadeText
+                className="max-w-[600px] md:text-xl"
+                delay={BLUR_FADE_DELAY}
+                text={DATA.description}
+              />
+            </div>
+            <BlurFade delay={BLUR_FADE_DELAY}>
+              <Avatar className="size-28 border">
+                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
+                <AvatarFallback>{DATA.initials}</AvatarFallback>
+              </Avatar>
+            </BlurFade>
+          </div>
+        </div>
+      </section>
+      <section id="about">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-xl font-bold">About</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+            {DATA.summary}
+          </Markdown>
+        </BlurFade>
+      </section>
+      <section id="work">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 5}>
+            <h2 className="text-xl font-bold">Work Experience</h2>
+          </BlurFade>
+          {DATA.work.map((work, id) => (
+            <BlurFade
+              key={work.company}
+              delay={BLUR_FADE_DELAY * 6 + id * 0.05}
+            >
+              <ResumeCard
+                key={work.company}
+                logoUrl={work.logoUrl}
+                altText={work.company}
+                title={work.company}
+                subtitle={work.title}
+                badges={work.badges}
+                period={`${work.start} - ${work.end ?? "Present"}`}
+                description={work.description}
+                bullets={work.bullets}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+
+      <section id="stack">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 9}>
+            <h2 className="text-xl font-bold">Tech Stack</h2>
+          </BlurFade>
+          <div className="flex flex-wrap gap-1">
+            {DATA.skills.map((skill, id) => (
+              <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+                <Badge key={skill}>{skill}</Badge>
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section id="blog">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <h2 className="text-xl font-bold">Articles</h2>
+          </BlurFade>
+          {posts
+            .sort((a, b) => {
+              if (
+                new Date(a.metadata.publishedAt) >
+                new Date(b.metadata.publishedAt)
+              ) {
+                return -1;
+              }
+              return 1;
+            })
+            .map((post, id) => (
+              <BlurFade
+                delay={BLUR_FADE_DELAY * 11 + id * 0.05}
+                key={post.slug}
+              >
+                <Link
+                  className="flex flex-col space-y-1 mb-4"
+                  href={`/blog/${post.slug}`}
+                >
+                  <div className="w-full flex flex-col">
+                    <p className="tracking-tight">{post.metadata.title}</p>
+                    <p className="h-6 text-xs text-muted-foreground">
+                      {post.metadata.publishedAt}
+                    </p>
+                  </div>
+                </Link>
+              </BlurFade>
+            ))}
+        </div>
+      </section>
+
+      <section id="contact">
+        <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 16}>
+            <div className="space-y-3">
+              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                Contact
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Get in Touch
+              </h2>
+              <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Send me an email at{" "}
+                <Link
+                  href={DATA.contact.social.Email.url}
+                  className="text-gray-500 hover:underline"
+                >
+                  ahmedhinedy@gmail.com
+                </Link>
+              </p>
+            </div>
+          </BlurFade>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/data/resume.tsx
@@ -0,0 +1,105 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Ahmed Hinedy",
+  initials: "AH",
+  url: "https://hinedy.github.io/portfolio",
+  location: "Alexandria, Egypt",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    "Aesthetics-focused and detail-oriented creative bringing a unique combination of analytical and creative skills to Frontend Development",
+  summary:
+    " Proficient in using NextJs and React with TypeScript. Self-driven developer with a strong ownership mindset, dedicated to staying up to date with the latest industry trends and technologies, consistently delivering innovative solutions to complex problems.",
+  avatarUrl: "/me.jpg",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Tailwind CSS",
+    "git",
+    "Figma",
+    "Adobe Photoshop",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "ahmedhinedy@gmail.com",
+    tel: "+201020790221",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/hinedy",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://linkedin.com/in/ahmedhinedy",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      Behance: {
+        name: "Behance",
+        url: "https://www.behance.net/ahmedhinedy",
+        icon: Icons.behance,
+
+        navbar: true,
+      },
+
+      Email: {
+        name: "Send Email",
+        url: "mailto:ahmedhinedy@gmail.com",
+        icon: Icons.email,
+
+        navbar: true,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "nWeave",
+      href: "",
+      badges: [],
+      location: "Alexandria, Egypt",
+      title: "Frontend Software Engineer",
+      logoUrl: "/nweave.png",
+      start: "July 2023",
+      end: "Present",
+      bullets: [
+        `Led frontend development efforts for a sophisticated medical services management platform, translating complex business requirements into implemented features, stabilizing the existing codebase, and coordinating tasks to successfully deliver a stable version within tight deadlines. `,
+        `Developed the frontend of a multi-tenant medical provider credentialing system, collaborating closely with the backend team.`,
+        `Established robust authentication and authorization mechanisms incorporating dynamic token rotation for enhanced security.`,
+        `Implemented dynamic forms using React Hook Form, streamlining the credentialing process and improving data entry accuracy.`,
+        `Built reusable components for tables, trees, tabs, form elements, and hooks significantly reducing development time and improving UI consistency.`,
+        `Optimized initial page fetching by migrating to server components, leveraging Next.js's server-side rendering capabilities to enhance performance and user experience.`,
+      ],
+      description: "",
+    },
+    {
+      company: "Freelance",
+      badges: [],
+      href: "",
+      location: "Remote",
+      title: "Software Engineer",
+      logoUrl: "/upwork.jpg",
+      start: "May 2023",
+      end: "Present",
+      bullets: [
+        `Delivered user interface enhancements, bug fixes, and integrations for Media & Entertainment and PropTech clients, working collaboratively on tight deadlines.`,
+        `Created interactive and responsive UI prototypes for a Proof of Concept (POC) using React and Framer Motion.`,
+        `Integrated Google Maps API into a real estate web application.`,
+      ],
+      description: "",
+    },
+  ],
+  education: [],
+  projects: [],
+  hackathons: [],
+} as const;
